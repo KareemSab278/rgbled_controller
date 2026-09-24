ae62bb40ccb8f5c137a53c9faf05cd51fa5197c3
kareemsab278 <kareemsab278@gmail.com>
2026-09-24 09:27:14 +0100

    forgot to call the code in main lmao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod rgb_led;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Running the LED controller now...");
+    rgb_led::set_color(rgb_led::Color::Red);
+    wait_sec(2);
+
+    rgb_led::set_color(rgb_led::Color::Green);
+    wait_sec(2);
+
+    rgb_led::set_color(rgb_led::Color::Blue);
+    wait_sec(2);
+
+    rgb_led::set_color(rgb_led::Color::Yellow);
+    wait_sec(2);
+
+    rgb_led::set_color(rgb_led::Color::White);
+    wait_sec(2);
+}
+
+
+fn wait_sec(secs: u8) -> () {
+    std::thread::sleep(std::time::Duration::from_secs(secs as u64))
 }
