1d0f86a4d38056ebf759df1818d86fcad995d1ea
kareemsab278 <kareemsab278@gmail.com>
2026-09-24 10:19:11 +0100

    attempting to work on 4b instead of 5

--- src/rgb_led.rs
+++ src/rgb_led.rs
@@ -1,5 +1,5 @@
 /*
-    module is meant to control WS2812B RGB LED Strips with the rasp pi 5 using embedded hal
+    module is meant to control WS2812B RGB LED Strips with the rasp pi 4b using embedded hal
     there is a good example of this here: https://github.com/rpi-ws281x/rpi-ws281x-rust/blob/master/examples/basic.rs
     using this module: https://crates.io/crates/rs_ws281x/0.5.1 (3 years old 🙏😭)
     max brightness is 255 min is 0. defaults to 255 is empty
@@ -9,7 +9,7 @@ use rs_ws281x::ControllerBuilder;
 use rs_ws281x::ChannelBuilder;
 use rs_ws281x::StripType;
 
-const LED_COUNT: u8 = 64;
+const LED_COUNT: u8 = 160;
 const GPIO_SPI0_MOSI_PIN: u8 = 10;
 
 #[derive(Clone, Copy, Debug)]
@@ -37,7 +37,7 @@ pub fn set_color(color: Color) -> Result<(), Box<dyn std::error::Error>> {
     println!("Setting color to {:?}", color);
     let mut controller = ControllerBuilder::new()
         .freq(800_000)
-        .dma(10)
+        .dma(5)
         .channel(
             0, // Channel Index
             ChannelBuilder::new()
