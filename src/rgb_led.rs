864bf7cf4d7f37464a53c08ea85bf2e867b3368f
kareemsab278 <kareemsab278@gmail.com>
2026-09-24 09:14:52 +0100

    replace gpio 10 to 16. pre test on rasp pi

--- src/rgb_led.rs
+++ src/rgb_led.rs
@@ -10,7 +10,7 @@ use rs_ws281x::ChannelBuilder;
 use rs_ws281x::StripType;
 
 const LED_COUNT: u8 = 64;
-const GPIO_SPI0_MOSI_PIN: u8 = 10;
+const GPIO_SPI0_MOSI_PIN: u8 = 16;
 
 enum Color {
     Red: [u8; 4],
