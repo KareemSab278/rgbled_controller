408cb934d0e4ea8668a923e925169436db632dcd
kareemsab278 <kareemsab278@gmail.com>
2026-09-24 10:39:22 +0100

    added longer timeout

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,24 @@ mod rgb_led;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Running the LED controller now...");
     rgb_led::set_color(rgb_led::Color::Red)?;
-    wait_sec(2);
+    wait(None);
 
     rgb_led::set_color(rgb_led::Color::Green)?;
-    wait_sec(2);
+    wait(None);
 
     rgb_led::set_color(rgb_led::Color::Blue)?;
-    wait_sec(2);
+    wait(None);
 
     rgb_led::set_color(rgb_led::Color::Yellow)?;
-    wait_sec(2);
+    wait(None);
 
     rgb_led::set_color(rgb_led::Color::White)?;
-    wait_sec(2);
+    wait(None);
 
     Ok(())
 }
 
-fn wait_sec(secs: u8) {
-    std::thread::sleep(std::time::Duration::from_secs(secs as u64));
+fn wait(secs: Option<u8>) {
+    let timeout: u64 = secs.unwrap_or(6) as u64;
+    std::thread::sleep(std::time::Duration::from_secs(timeout));
 }
