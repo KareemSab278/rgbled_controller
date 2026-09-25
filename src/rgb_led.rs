25e566c3d645a3a7635239baf7fc919918e0f8e9
kareemsab278 <kareemsab278@gmail.com>
2026-09-25 14:55:49 +0100

    use 21

--- src/rgb_led.rs
+++ src/rgb_led.rs
@@ -1,12 +1,12 @@
 /*
-    module controls WS2812B RGB LED strips on Raspberry Pi 5 using SPI
-    SPI10 MOSI is used because GPIO10 (SPI0 MOSI) is occupied.
-
-    DIN is wired to GPIO21 (physical pin 40)
+    module is meant to control WS2812B RGB LED Strips with the rasp pi 5 using SPI
+    (rs_ws281x relies on PWM/DMA which is unsupported on the Pi 5, hence SPI via ws2812-spi)
+    DIN is wired to GPIO20 (SPI1 MOSI, physical pin 38) since GPIO10/SPI0 is used elsewhere
+    requires dtoverlay=spi1-1cs (or spi1-3cs) enabled in /boot/firmware/config.txt
 */
 
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
-use smart_leds_trait::{RGB8, SmartLedsWrite};
+use smart_leds_trait::{SmartLedsWrite, RGB8};
 use ws2812_spi::Ws2812;
 
 const LED_COUNT: usize = 148;
@@ -26,16 +26,8 @@ impl Color {
             Color::Red => RGB8 { r: 255, g: 0, b: 0 },
             Color::Green => RGB8 { r: 0, g: 255, b: 0 },
             Color::Blue => RGB8 { r: 0, g: 0, b: 255 },
-            Color::White => RGB8 {
-                r: 255,
-                g: 255,
-                b: 255,
-            },
-            Color::Yellow => RGB8 {
-                r: 255,
-                g: 255,
-                b: 0,
-            },
+            Color::White => RGB8 { r: 255, g: 255, b: 255 },
+            Color::Yellow => RGB8 { r: 255, g: 255, b: 0 },
         }
     }
 }
@@ -43,15 +35,12 @@ impl Color {
 pub fn set_color(color: Color) -> Result<(), Box<dyn std::error::Error>> {
     println!("Setting color to {:?}", color);
 
-    let spi = Spi::new(Bus::Spi10, SlaveSelect::Ss0, 6_400_000, Mode::Mode0)?;
-
+    let spi = Spi::new(Bus::Spi1, SlaveSelect::Ss0, 3_000_000, Mode::Mode0)?;
     let mut controller = Ws2812::new(spi);
 
     let leds = vec![color.to_rgb8(); LED_COUNT];
-
     controller.write(leds.into_iter())?;
 
     println!("Colors set to {:?}", color);
-
     Ok(())
 }
